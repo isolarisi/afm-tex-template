6527705c8f6e710d4e002b6bca33ff487af7b734
Ivan Rebro <rii@bk.ru>
2018-07-24 08:29:54 +0300

    Добавлены скрипты быстрого доступа

---
+++ scripts/italic.js
@@ -0,0 +1,30 @@
+// TeXworksScript
+// Title: Italic
+// Shortcut: Ctrl+Shift+k
+// Description: Encloses the current selection in {\sl}
+// Author: Rebro Ivan
+// Version: 0.1
+// Date: 2017-10-26
+// Script-Type: standalone
+// Context: TeXDocument
+
+function addOrRemove(prefix, suffix) {
+  var txt = TW.target.selection;
+  var len = txt.length;
+  var wrapped = prefix + txt + suffix;
+  var pos = TW.target.selectionStart;
+  if (pos >= prefix.length) {
+    TW.target.selectRange(pos - prefix.length, wrapped.length);
+    if (TW.target.selection == wrapped) {
+      TW.target.insertText(txt);
+      TW.target.selectRange(pos - prefix.length, len);
+      return;
+    }
+    TW.target.selectRange(pos, len);
+  }
+  TW.target.insertText(wrapped);
+  TW.target.selectRange(pos + prefix.length, len);
+  return;
+};
+
+addOrRemove("{\\sl", "}");
---
+++ scripts/quotes.js
@@ -0,0 +1,30 @@
+// TeXworksScript
+// Title: Quotes
+// Shortcut: Ctrl+Shift+2
+// Description: Encloses the current selection in <<>>
+// Author: Rebro Ivan
+// Version: 0.1
+// Date: 2017-10-26
+// Script-Type: standalone
+// Context: TeXDocument
+
+function addOrRemove(prefix, suffix) {
+  var txt = TW.target.selection;
+  var len = txt.length;
+  var wrapped = prefix + txt + suffix;
+  var pos = TW.target.selectionStart;
+  if (pos >= prefix.length) {
+    TW.target.selectRange(pos - prefix.length, wrapped.length);
+    if (TW.target.selection == wrapped) {
+      TW.target.insertText(txt);
+      TW.target.selectRange(pos - prefix.length, len);
+      return;
+    }
+    TW.target.selectRange(pos, len);
+  }
+  TW.target.insertText(wrapped);
+  TW.target.selectRange(pos + prefix.length, len);
+  return;
+};
+
+addOrRemove("<<",">>");
